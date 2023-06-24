eb50719548b9a9af0b692749c5a355801c3eb4db
Hemanth Alluri <hdrive1999@gmail.com>
2023-06-24 13:18:19 -0700

    49. Group Anagrams

---
+++ neetcode/75/arrays-and-hashing/04/MySolution.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class MySolution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> groups = new HashMap<>();
+        for (String s : strs) {
+            String signature = getSignature(s);
+            List<String> group = groups.get(signature);
+            if (group == null) {
+                group = new ArrayList<String>();
+                groups.put(signature, group);
+            }
+            group.add(s);
+        }
+        return new ArrayList<>(groups.values());
+    }
+
+    String getSignature(String in) {
+        Integer[] signature = new Integer[26];
+        for (int i = 0; i < 26; i++) {
+            signature[i] = 0;
+        }
+        for (int i = 0; i < in.length(); i++) {
+            signature[in.charAt(i) - 'a']++;
+        }
+        return Arrays.asList(signature).stream()
+                .map(x -> String.valueOf(x))
+                .collect(Collectors.joining(","));
+    }
+}
