0a61d6d566f361529582880499b3939e50bd780f
Hemanth Alluri <hdrive1999@gmail.com>
2023-06-25 16:54:54 -0700

    347. Top K Frequent Elements

---
+++ neetcode/75/arrays-and-hashing/05/MySolution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+class MySolution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            int frequency = frequencyMap.getOrDefault(num, 0);
+            frequencyMap.put(num, frequency + 1);
+        }
+
+        List<Entry<Integer, Integer>> frequencyPairs = new ArrayList<>(frequencyMap.entrySet());
+        frequencyPairs.sort((e1, e2) -> e2.getValue() - e1.getValue());
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = frequencyPairs.get(i).getKey();
+        }
+
+        return result;
+    }
+}
