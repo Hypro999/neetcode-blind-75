75dc8c188ffa5003fe2dcfb2686398c08dbffbde
Hemanth Alluri <hdrive1999@gmail.com>
2023-08-07 07:25:52 -0700

    121. Best Time to Buy and Sell Stock

---
+++ neetcode/75/sliding-window/01/MySolution.java
@@ -0,0 +1,19 @@
+public class MySolution {
+
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int start = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            int end = prices[i];
+            if (end < start) {
+                start = end;
+            } else {
+                int profit = end - start;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ neetcode/75/sliding-window/01/README.md
@@ -0,0 +1,8 @@
+# 121. Best Time to Buy and Sell Stock
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+Topics:
+
+-   Sliding Window
+-   Arrays
