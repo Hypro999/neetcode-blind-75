35c9a53048afe93b1d93c9bc9bea39aae5a5af26
Hemanth Alluri <hdrive1999@gmail.com>
2024-06-08 11:35:06 -0700

    295. Find Median from Data Stream
    Improve solution - make it much more readable.

--- neetcode/75/heap/01/solution.py
+++ neetcode/75/heap/01/solution.py
@@ -2,33 +2,32 @@ import heapq
 
 
 class MedianFinder:
-    def __init__(self) -> None:
-        self.head: int = None
-        self.left: list[int] = []  # max heap as a min heap of inverted numbers
-        self.right: list[int] = []  # min heap
+
+    def __init__(self):
+        self.left = []  # max heap as a min heap of inverted numbers
+        self.right = []  # min heap
 
     def addNum(self, num: int) -> None:
-        if self.head is None:
-            self.head = num
+        if len(self.left) == 0:
+            self.left.append(-num)
             return
-        if num < self.head:
-            if len(self.left) > len(self.right):
-                heapq.heappush(self.right, self.head)
-                self.head = -heapq.heappushpop(self.left, -num)
-            else:
-                heapq.heappush(self.left, -num)
+
+        # Put the number into the correct heap
+        left_val = -self.left[0]
+        if num < left_val:
+            heapq.heappush(self.left, -num)
         else:
-            if len(self.right) > len(self.left):
-                heapq.heappush(self.left, -self.head)
-                self.head = heapq.heappushpop(self.right, num)
-            else:
-                heapq.heappush(self.right, num)
+            heapq.heappush(self.right, num)
+
+        # Rebalance heaps as needed
+        if len(self.left) > len(self.right) + 1:
+            transfer_val = -heapq.heappop(self.left)
+            heapq.heappush(self.right, transfer_val)
+        elif len(self.right) > len(self.left):
+            transfer_val = heapq.heappop(self.right)
+            heapq.heappush(self.left, -transfer_val)
 
     def findMedian(self) -> float:
         if len(self.left) == len(self.right):
-            return self.head
-        if len(self.left) > len(self.right):
-            top = -self.left[0]
-        else:
-            top = self.right[0]
-        return float(self.head + top) / 2
+            return (-self.left[0] + self.right[0]) / 2
+        return -self.left[0]
