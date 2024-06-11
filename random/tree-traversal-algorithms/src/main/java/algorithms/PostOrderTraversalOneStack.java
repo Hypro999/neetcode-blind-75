6a3f34c69dca1fadb4b83603deb08efc355d80ce
Hemanth Alluri <hdrive1999@gmail.com>
2024-06-10 18:27:13 -0700

    Implement PostOrderTraversalOneStack.

---
+++ random/tree-traversal-algorithms/src/main/java/algorithms/PostOrderTraversalOneStack.java
@@ -0,0 +1,40 @@
+package algorithms;
+
+import structures.TreeNode;
+
+import java.util.Stack;
+import java.util.function.Consumer;
+
+public class PostOrderTraversalOneStack<T> implements TraversalStrategy<T> {
+
+    Stack<TreeNode<T>> stack;
+
+    public PostOrderTraversalOneStack() {
+        this.stack = new Stack<>();
+    }
+
+    @Override
+    public void traverse(TreeNode<T> root, Consumer<T> action) {
+        exahustLeft(root);
+        while (!stack.isEmpty()) {
+            TreeNode<T> top = stack.pop();
+            if (!stack.isEmpty() && stack.peek().equals(top.right)) {
+                TreeNode<T> right = stack.pop();
+                stack.push(top);
+                exahustLeft(right);
+            } else {
+                action.accept(top.value);
+            }
+        }
+    }
+
+    private void exahustLeft(TreeNode<T> root) {
+        while (root != null) {
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+            stack.push(root);
+            root = root.left;
+        }
+    }
+}
